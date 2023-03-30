889dee558a65a2c5d964dc6cb9942b83801d6972
Modus <mauricehess@hotmail.de>
2023-03-30 12:48:16 +0200

    started with lobby class - fields and methods

--- src/main/java/ch/uzh/ifi/hase/soprafs23/model/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/model/Lobby.java
@@ -1,24 +1,43 @@
 package ch.uzh.ifi.hase.soprafs23.model;
 
+import ch.uzh.ifi.hase.soprafs23.constant.FoodCategory;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import ch.uzh.ifi.hase.soprafs23.constant.FoodCategory;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 
 
 @AllArgsConstructor
 @Data
 public class Lobby {
     private String user_name;
-    private final String game_code;
+    private final String gameCode;
     private final User host;
+    private Integer roundLimit;
+    private FoodCategory foodCategory;
+    private ArrayList<User> players;
 
     public Lobby(User hostUser) {
         CodeGenerator codeGenerator = new CodeGenerator();
-        game_code = codeGenerator.nextCode();
+        gameCode = codeGenerator.nextCode();
         host = hostUser;
+    }
 
+    public void addPlayer(User user){
+        players.add(user);
+    }
+
+    public void removePlayer(User user) {
+        players.remove(user);
+    }
+
+    public void playGame(){
 
     }
+
+
 }
