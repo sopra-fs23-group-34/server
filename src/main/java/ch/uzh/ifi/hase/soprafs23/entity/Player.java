4c9a731181da7a2bf05ca3d4c8d8f2796153fd5e
Modus <mauricehess@hotmail.de>
2023-03-31 11:39:18 +0200

    added Player class, fields & methods to be done

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Player.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs23.entity;
+
+import ch.uzh.ifi.hase.soprafs23.model.Lobby;
+
+public class Player {
+    private boolean isHost;
+    private Integer place;
+
+
+    public Player(LobbyPlayer user) {
+
+    }
+}
