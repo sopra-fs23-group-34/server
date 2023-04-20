132965dac2b8bb8525c604ca0d6fc0ae9f53cc4a
Modus <mauricehess@hotmail.de>
2023-04-20 14:10:39 +0200

    added ID to foods entity

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Foods.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Foods.java
@@ -25,6 +25,10 @@ public class Foods implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
     @Column (nullable = false)
     private String name;
 
