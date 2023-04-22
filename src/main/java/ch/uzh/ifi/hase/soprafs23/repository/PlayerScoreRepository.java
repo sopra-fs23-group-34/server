1eb71a81533206e2714a78d395e41f2bfeac91b1
nataell95 <nataell.cornu@uzh.ch>
2023-04-22 23:17:01 +0200

    returning also usernames for gloabal leaderboard

--- src/main/java/ch/uzh/ifi/hase/soprafs23/repository/PlayerScoreRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/PlayerScoreRepository.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.hase.soprafs23.repository;
 
 import ch.uzh.ifi.hase.soprafs23.constant.FoodCategory;
 import ch.uzh.ifi.hase.soprafs23.entity.Foods;
+import ch.uzh.ifi.hase.soprafs23.entity.LeaderBoard;
 import ch.uzh.ifi.hase.soprafs23.entity.PlayerScore;
 import ch.uzh.ifi.hase.soprafs23.model.Player;
 import com.mysql.cj.xdevapi.SqlResult;
@@ -16,6 +17,8 @@ import java.util.Map;
 
 @Repository("playerScores")
 public interface PlayerScoreRepository extends JpaRepository<PlayerScore, Long> {
-    @Query(value = "SELECT player_id as user_id, SUM(score) AS total_score FROM player_scores GROUP BY player_id ORDER BY total_score DESC", nativeQuery = true)
-    List<Map<String, Integer>> getGlobalLeaderboard();
+    @Query("SELECT NEW ch.uzh.ifi.hase.soprafs23.entity.LeaderBoard(ps.player_id, SUM(ps.score)) FROM PlayerScore ps GROUP BY ps.player_id ORDER BY SUM(ps.score) DESC")
+    List<LeaderBoard> getGlobalLeaderboard();
 }
+
+
