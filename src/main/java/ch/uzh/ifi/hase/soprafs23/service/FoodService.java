d453f50700d96daeb69254e34aac98e61a58709b
Modus <mauricehess@hotmail.de>
2023-04-23 10:42:08 +0200

    closes #85

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/FoodService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/FoodService.java
@@ -68,38 +68,35 @@ public class FoodService {
         return randomFoods;
     }
 
-
-
-    public static Food getFood(String food_name) throws IOException {
-        String apiUrl = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-        //String appId = "9dd751e9";
-        //String appKey = "7470f45a98ccc467dc3c043b1f997cf4";
-        String appId = "376a71b1";
-        String appKey = "46ef2c8c088e63f038d5b2e0d43cf066";
-        String remoteUser = "0";
-        String query = food_name;
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("x-app-id", appId);
-        headers.set("x-app-key", appKey);
-        headers.set("x-remote-user-id", remoteUser);
-        Map<String, String> body = new HashMap<>();
-        body.put("query", query);
-        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
-        String responseBody = response.getBody();
+    public Food getFood(String food_name) throws IOException {
+        List<String[]> apiKeys = new ArrayList<>();
+        apiKeys.add(new String[]{"9dd751e9","7470f45a98ccc467dc3c043b1f997cf4", "0"});
+        apiKeys.add(new String[]{"9dd751e9","7470f45a98ccc467dc3c043b1f997cf4", "1"});
+        apiKeys.add(new String[]{"376a71b1","46ef2c8c088e63f038d5b2e0d43cf066", "0"});
+        apiKeys.add(new String[]{"376a71b1","46ef2c8c088e63f038d5b2e0d43cf066", "1"});
+        int maxTries = apiKeys.size();
+        int tries = 0;
+        String responseBody = "";
+        while (true) {
+            try {
+                String appId = apiKeys.get(tries)[0];
+                String appKey = apiKeys.get(tries)[1];
+                String remoteUser = apiKeys.get(tries)[2];
+                responseBody = apiCall(food_name, appId, appKey, remoteUser);
+                break;
+            } catch (Exception e) {
+                tries++;
+                if (tries >= maxTries) {
+                    throw new RuntimeException("no more API calls available for today");
+                }
+            }
+        }
 
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map = objectMapper.readValue(responseBody, Map.class);
         List<Map<String, Object>> foods = (List<Map<String, Object>>) map.get("foods");
         Map<String, Object> food = foods.get(0);
         Map<String, String> photo = (Map<String, String>) food.get("photo");
-        //System.out.println(food.get("nf_calories").getClass());
-        //System.out.println(food.get("nf_total_fat").getClass());
-        //System.out.println(food.get("nf_protein").getClass());
-        //System.out.println(food.get("nf_total_carbohydrate").getClass());
-        //System.out.println(food.get("----------"));
         Number calories = (Number) food.get("nf_calories");
         Number fat = (Number) food.get("nf_total_fat");
         Number protein = (Number) food.get("nf_protein");
@@ -107,13 +104,13 @@ public class FoodService {
         String name = (String) food.get("food_name");
         String image_link = photo.get("highres");
 
-        System.out.println("name: " + name);
+        //System.out.println("name: " + name);
         //System.out.println("calories: " + calories);
         //System.out.println("fat: " + fat);
         //System.out.println("protein: " + protein);
         //System.out.println("carbs: " + carbs);
-        System.out.println("image link: " + image_link);
-        System.out.println("----------");
+        //System.out.println("image link: " + image_link);
+        //System.out.println("----------");
 
         Map<String, Double> nutritional_values = new HashMap<>();
         nutritional_values.put("calories", calories.doubleValue());
@@ -121,17 +118,22 @@ public class FoodService {
         nutritional_values.put("protein", protein.doubleValue());
         nutritional_values.put("carbs", carbs.doubleValue());
 
-
         Food apiFood = new Food(name, nutritional_values, image_link);
         return apiFood;
         }
 
-    public static void main(String[] args) throws IOException {
-        String[] foods = new String[] {
-                "Butter"};
-        for (String food : foods) {
-            FoodService.getFood(food);
+        private String apiCall(String foodName, String appId, String appKey, String remoteUser) {
+            String apiUrl = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("x-app-id", appId);
+            headers.set("x-app-key", appKey);
+            headers.set("x-remote-user-id", remoteUser);
+            Map<String, String> body = new HashMap<>();
+            body.put("query", foodName);
+            HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(body, headers);
+            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
+            return response.getBody();
         }
-
-    }
 }
