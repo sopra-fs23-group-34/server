304a296b93141d910eac85fe81fd3ecd7baebcd0
Modus <mauricehess@hotmail.de>
2023-04-27 13:51:03 +0200

    LobbyControllerTest

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyControllerTest.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
+import ch.uzh.ifi.hase.soprafs23.model.GameConfig;
 import ch.uzh.ifi.hase.soprafs23.service.LobbyService;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -13,11 +14,12 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(LobbyController.class)
 public class LobbyControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -28,7 +30,7 @@ public class LobbyControllerTest {
     @MockBean
     private  LobbyService lobbyService;
 
-/*
+
     @Test
     public void createLobby_successful() throws Exception {
         String code = "abcdef";
@@ -37,13 +39,20 @@ public class LobbyControllerTest {
         MockHttpServletRequestBuilder postRequest = post("/lobbys/create")
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("id", "1");
+
         System.out.println(postRequest);
         mockMvc.perform(postRequest)
                 .andExpect(status().isCreated());
-
     }
 
- */
+    @Test
+    public void joinLobby_successful() throws Exception {
+        given(lobbyService.joinLobby(Mockito.any(), Mockito.any())).willReturn(true);
 
-
+        MockHttpServletRequestBuilder postRequest = post("/lobbys/join/1/1")
+                .contentType(MediaType.APPLICATION_JSON);
+        System.out.println(postRequest);
+        mockMvc.perform(postRequest)
+                .andExpect(status().isOk());
+    }
 }
