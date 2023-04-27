18c35092dedeba9fa436d395ddb4a1ef9a0ccca7
Modus <mauricehess@hotmail.de>
2023-04-27 16:30:34 +0200

    tests

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/FoodServiceTest.java
@@ -0,0 +1,66 @@
+package ch.uzh.ifi.hase.soprafs23.service;
+import ch.uzh.ifi.hase.soprafs23.constant.FoodCategory;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.model.Food;
+import ch.uzh.ifi.hase.soprafs23.repository.FoodsRepository;
+import ch.uzh.ifi.hase.soprafs23.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FoodServiceTest {
+    @Mock
+    private FoodsRepository foodRepository;
+
+    @InjectMocks
+    private FoodService foodService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getRandomFoods() throws IOException {
+        List<String> f = new ArrayList<>();
+        f.add("Apple");
+        f.add("Banana");
+        f.add("Lemon");
+        f.add("Grapes");
+        f.add("Strawberry");
+        when(foodService.getRandomFoods(5, FoodCategory.FRUITS)).thenReturn(f);
+        List<String> foods = foodService.getRandomFoods(5, FoodCategory.FRUITS);
+        assertEquals(foods.size(), 5);
+    }
+
+    @Test
+    public void APIcall() throws IOException {
+        Map<String, Double> nutritionalValues = new HashMap<>();
+        nutritionalValues.put("carbs", 13.807692307692307);
+        nutritionalValues.put("protein", 0.2582417582417582);
+        nutritionalValues.put("fat", 0.1703296703296703);
+        nutritionalValues.put("calories", 52.0);
+        Food apple = new Food("apple", nutritionalValues, "https://nix-tag-images.s3.amazonaws.com/384_highres.jpg");
+
+        Food food = foodService.getFood("apple");
+        assertEquals(food.getName(), apple.getName());
+        assertEquals(food.getNutritionValues(), apple.getNutritionValues());
+        assertEquals(food.getImage(), apple.getImage());
+    }
+
+}
