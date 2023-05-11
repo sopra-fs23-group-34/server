6efb2ed193ee3fc0acff839c73c3978b393560f2
Modus <mauricehess@hotmail.de>
2023-05-11 14:33:31 +0200

    tests

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/PlayerStatisticsTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/PlayerStatisticsTest.java
@@ -13,14 +13,17 @@ public class PlayerStatisticsTest {
     @Test
     public void testConstructorAndGetters() {
         Long userId = 1234L;
-        Long gamesPlayed = 5678L;
+        long gamesPlayed = 5678L;
         int highScore = 23;
         long gamesWon = 1;
         double winRatio = 0.1;
 
         PlayerStatistics playerStatistics = new PlayerStatistics(userId,gamesPlayed,highScore,gamesWon,winRatio);
         assertEquals(userId, playerStatistics.getUserId());
+        assertEquals(gamesPlayed, playerStatistics.getGamesPlayed());
+        assertEquals(highScore, playerStatistics.getHighScore());
         assertEquals(gamesWon, playerStatistics.getGamesWon());
+        assertEquals(winRatio, playerStatistics.getWinRatio());
 
     }
 
